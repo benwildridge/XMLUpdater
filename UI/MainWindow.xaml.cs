6c655f24c2037e80d36f5ffab98f31945f2ac10d
Ben Wildridge <ben.wildridge@gmail.com>
2021-05-13 22:14:49 +0100

    Updated XMLUpdate to add method and replace numerous lines of code

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -21,8 +21,18 @@ namespace UI
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    //TODO - Update window to be Howdenised
+    //TODO - Block update button if no tickboxes are checked
+    //TODO - Add error handling if the XML is not in the correct structure and output to log file.
+
+
     public partial class MainWindow : Window
     {
+
+        private string currentBoardCode = "";
+        private string newBoardCode = "";
+
+
         public static void VerifyDir(string path)
         {
             try
@@ -34,6 +44,44 @@ namespace UI
                 }
             }
             catch { }
+        }
+
+        public void XMLUpdate()
+        {
+
+            List<string> files = new List<string>();
+
+            if ((bool)checkBoxSTD.IsChecked)
+            {
+                files.AddRange(System.IO.Directory.GetFiles(System.Configuration.ConfigurationManager.AppSettings["STD"], "*.xml").ToList());
+            }
+            if((bool)checkBoxWest.IsChecked)
+            {
+                files.AddRange(System.IO.Directory.GetFiles(System.Configuration.ConfigurationManager.AppSettings["WEST"], "*.xml").ToList());
+            }
+            if((bool)checkBoxMultiW.IsChecked)
+            {
+                files.AddRange(System.IO.Directory.GetFiles(System.Configuration.ConfigurationManager.AppSettings["MULTIW"], "*.xml").ToList());
+            }
+            if ((bool)checkBoxMultIE.IsChecked)
+            {
+                files.AddRange(System.IO.Directory.GetFiles(System.Configuration.ConfigurationManager.AppSettings["MULTIE"], "*.xml").ToList());
+            }
+                foreach (string file in files)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(file);
+                XmlElement root = (XmlElement)doc.DocumentElement.SelectSingleNode("//CutList/Board");
+                string XMLCurrentBoardCode = root.GetAttribute("BrdCode");
+
+                if (XMLCurrentBoardCode == currentBoardCode)
+                {
+                    root.SetAttribute("BrdCode", $"{newBoardCode}");
+                    Logger($"{file} " + "was updated");
+                    doc.Save(file);
+                }
+
+            }
         }
 
         public static void Logger(string lines)
@@ -48,103 +96,6 @@ namespace UI
                 file.Close();
             }
             catch (Exception) { }
-        }
-
-        private string currentBoardCode = "";
-        private string newBoardCode = "";
-
-        
-        //TODO File Change Logic Here
-        public void UpdateFiles()
-        {
-            if ((bool)checkBoxSTD.IsChecked)
-            {
-                string xmlFilePath = System.Configuration.ConfigurationManager.AppSettings["STD"];
-
-                var files = System.IO.Directory.GetFiles(xmlFilePath, "*.xml");
-                foreach (string file in files)
-                {
-                   XmlDocument doc = new XmlDocument();
-                   doc.Load(file);
-                   XmlElement root = (XmlElement)doc.DocumentElement.SelectSingleNode("//CutList/Board");
-                   string XMLCurrentBoardCode = root.GetAttribute("BrdCode");
-
-                    if (XMLCurrentBoardCode == currentBoardCode)
-                        {
-                       root.SetAttribute("BrdCode", $"{newBoardCode}");
-                       Logger($"{file} " + "was updated");
-                       doc.Save(file);
-                    }
-
-                }
-            }
-            if ((bool)checkBoxWest.IsChecked)
-            {
-                string xmlFilePath = System.Configuration.ConfigurationManager.AppSettings["WEST"];
-
-                var files = System.IO.Directory.GetFiles(xmlFilePath, "*.xml");
-                foreach (string file in files)
-                {
-                    XmlDocument doc = new XmlDocument();
-                    doc.Load(file);
-                    XmlElement root = (XmlElement)doc.DocumentElement.SelectSingleNode("//CutList/Board");
-                    string XMLCurrentBoardCode = root.GetAttribute("BrdCode");
-
-                    if (XMLCurrentBoardCode == currentBoardCode)
-                    {
-                        root.SetAttribute("BrdCode", $"{newBoardCode}");
-                        Logger($"{file} " + "was updated");
-                        doc.Save(file);
-                    }
-
-
-                    
-                }
-            }
-            if ((bool)checkBoxMultIE.IsChecked)
-            {
-                string xmlFilePath = System.Configuration.ConfigurationManager.AppSettings["MULTIE"];
-
-                var files = System.IO.Directory.GetFiles(xmlFilePath, "*.xml");
-                foreach (string file in files)
-                {
-                    XmlDocument doc = new XmlDocument();
-                    doc.Load(file);
-                    XmlElement root = (XmlElement)doc.DocumentElement.SelectSingleNode("//CutList/Board");
-                    string XMLCurrentBoardCode = root.GetAttribute("BrdCode");
-
-                    if (XMLCurrentBoardCode == currentBoardCode)
-                    {
-                        root.SetAttribute("BrdCode", $"{newBoardCode}");
-                        Logger($"{file} " + "was updated");
-                        doc.Save(file);
-                    }
-
-                    
-                }
-            }
-            if ((bool)checkBoxMultiW.IsChecked)
-            {
-                string xmlFilePath = System.Configuration.ConfigurationManager.AppSettings["MULTIW"];
-
-                var files = System.IO.Directory.GetFiles(xmlFilePath, "*.xml");
-                foreach (string file in files)
-                {
-                    XmlDocument doc = new XmlDocument();
-                    doc.Load(file);
-                    XmlElement root = (XmlElement)doc.DocumentElement.SelectSingleNode("//CutList/Board");
-                    string XMLCurrentBoardCode = root.GetAttribute("BrdCode");
-
-                    if (XMLCurrentBoardCode == currentBoardCode)
-                    {
-                        root.SetAttribute("BrdCode", $"{newBoardCode}");
-                        Logger($"{file} " + "was updated");
-                        doc.Save(file);
-                    }
-
-                    
-                }
-            }
         }
 
         public MainWindow()
@@ -195,8 +146,9 @@ namespace UI
                 case MessageBoxResult.Yes:
                     currentBoardCode = currentBoardCodeTextBox.Text;
                     newBoardCode = newBoardCodeTextBox.Text;
-                    UpdateFiles();
-                    MessageBox.Show("Update Complete. Check C:\\Temp\\Log for details of files updated" + Environment.NewLine + "You can now close the application.");
+                    XMLUpdate();
+                    //TODO - Update Message Box to show nicer
+                    MessageBox.Show("Update Complete. Check C:\\Temp\\Log for details of files updated." + Environment.NewLine + "You can now close the application.");
                     break;
 
                 case MessageBoxResult.No:
